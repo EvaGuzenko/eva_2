01010efcc571f12fd4c9d588fc21b1236da12e78
EvaGuzenko <32966846+EvaGuzenko@users.noreply.github.com>
2017-10-29 00:50:03 +0300

    Add files via upload

---
+++ main22.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int power(long int x, unsigned int y);
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+cout << power(a, b) << endl;
+
+	return 0;
+}
+
+int power(long int x, unsigned int y)
+{
+	int d = 0;
+	if (y == 0)
+		d = 1;
+	else if (y == 1)
+		d = x;
+	else if (y % 2 == 0)
+		d = power (x * x, y/2);
+	else
+		d = x * power(x * x, y/2);
+	return d;
+}
